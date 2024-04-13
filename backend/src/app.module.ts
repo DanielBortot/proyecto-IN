820335ad12d5aac1f68974d4286379537e731fa7
Daniel Bortot <daniel.bortot@gmail.com>
2024-04-12 20:53:46 -0400

    Configuracion typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (env: ConfigService) => ({
+        type: 'postgres',
+        host: env.getOrThrow('POSTGRES_HOST'),
+        port: +env.getOrThrow('POSTGRES_PORT'),
+        username: env.getOrThrow('POSTGRES_USER'),
+        password: env.getOrThrow('POSTGRES_PASSWORD'),
+        database: env.getOrThrow('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true,
+
+      }),
+    })
+  ],
   controllers: [],
   providers: [],
 })
