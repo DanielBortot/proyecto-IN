9e09167098cd8186837f8812e6519567f99efaf3
Daniel Bortot <daniel.bortot@gmail.com>
2024-06-06 09:05:55 -0400

    feat: :beers: resto de entidades

--- datawarehouse/src/app.module.ts
+++ datawarehouse/src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EntidadesModule } from './entidades/entidades.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [EntidadesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (env: ConfigService) => ({
+          type: 'postgres',
+          host: env.getOrThrow('POSTGRES_HOST'),
+          port: +env.getOrThrow('POSTGRES_PORT'),
+          username: env.getOrThrow('POSTGRES_USER'),
+          password: env.getOrThrow('POSTGRES_PASSWORD'),
+          database: env.getOrThrow('POSTGRES_DB'),
+          entities: [__dirname + '/src/*/.entity{.ts,.js}'],
+          synchronize: true,
+      }),
+    }),
+    EntidadesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
